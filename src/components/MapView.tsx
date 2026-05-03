45eb7a841792c12a232106b978345aa891914416
Maksim Puzyrkov <maksim.puzyrkov@volvocars.com>
2026-05-03 18:02:52 +0200

    refactor: remove unused useMemo import from MapView component

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback, useMemo } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import { useStore } from '../store';
